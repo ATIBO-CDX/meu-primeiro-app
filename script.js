dcc74f78881ffd9ff1426902a9d9a2fd401a0fa1
ATIBO-CDX <atibodamop@gmail.com>
2026-05-24 20:48:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,28 @@ interactiveCards.forEach((card) => {
     card.style.setProperty('--my', `${y}%`);
   });
 });
+
+// FAQ INTERATIVO
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+const button = item.querySelector(".faq-question");
+
+button.addEventListener("click", () => {
+
+const activeItem =
+document.querySelector(".faq-item.active");
+
+if(activeItem && activeItem !== item){
+
+activeItem.classList.remove("active");
+
+}
+
+item.classList.toggle("active");
+
+});
+
+});
