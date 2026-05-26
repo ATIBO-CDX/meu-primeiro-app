5f1e4e57541dc990824475712b02d77624bdf6d1
ATIBO-CDX <atibodamop@gmail.com>
2026-05-26 21:03:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -172,12 +172,23 @@ backToTop.classList.remove(
 }
 );
 
-backToTop.addEventListener(
-"click",
-function(){
-window.scrollTo({
-top:0,
-behavior:"smooth"
+<script>
+const backToTop =
+document.getElementById("backToTop");
+window.addEventListener("scroll", function(){
+    if(window.scrollY > 300){
+        backToTop.style.display = "block";
+
+    }else{
+
+        backToTop.style.display = "none";
+    }
 });
+  
+backToTop.addEventListener("click", function(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    });
 });
-}
+</script>
